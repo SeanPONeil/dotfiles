7f22cccd9f30e13f036e2247fd814d5993160ea6
Sean O'Neil <oneilse14@gmail.com>
2020-11-21 19:37:25 -0800

    alacritty

---
+++ .config/alacritty/launch.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+MBP_RETINA="67702892-17FD-6076-2C69-5FFBA2BAB607"
+LG="3C42190A-8DAB-6B58-E6BC-6AF6739F4409"
+
+query=$(yabai -m query --spaces)
+focused_space=$(echo $query | jq '.[] | select(.focused == 1)')
+display_index=$(echo $focused_space | jq '.display')
+
+display=$(yabai -m query --displays | jq ".[] | select(.index == $display_index)")
+uuid=$(echo $display | jq -r '.uuid')
+
+if [[ $uuid == $MBP_RETINA ]]; then
+  cd $HOME && alacritty --config-file $HOME/.config/alacritty/alacritty-retina.yml
+else
+  cd $HOME && alacritty
+fi
