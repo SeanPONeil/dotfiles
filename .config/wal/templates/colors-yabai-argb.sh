6aceadc6b25310a7b0f09ca12ffc2aa3ccd23c87
Sean O'Neil <oneilse14@gmail.com>
2020-12-15 18:48:42 -0800

    yabai

--- .config/wal/templates/colors-yabai-argb.sh
+++ .config/wal/templates/colors-yabai-argb.sh
@@ -3,25 +3,25 @@
 wallpaper={wallpaper}
 
 # Special
-background=0x{background.argb_strip}
-foreground=0x{foreground.argb_strip}
-cursor=0x{cursor.strip}
+background_argb=0x{background.argb_strip}
+foreground_argb=0x{foreground.argb_strip}
+cursor_argb=0x{cursor.strip}
 alpha={alpha.alpha_dec}
 
 # Colors
-color0=0x{color0.argb_strip}
-color1=0x{color1.argb_strip}
-color2=0x{color2.argb_strip}
-color3=0x{color3.argb_strip}
-color4=0x{color4.argb_strip}
-color5=0x{color5.argb_strip}
-color6=0x{color6.argb_strip}
-color7=0x{color7.argb_strip}
-color8=0x{color8.argb_strip}
-color9=0x{color9.argb_strip}
-color10=0x{color10.argb_strip}
-color11=0x{color11.argb_strip}
-color12=0x{color12.argb_strip}
-color13=0x{color13.argb_strip}
-color14=0x{color14.argb_strip}
-color15=0x{color15.argb_strip}
+color0_argb=0x{color0.argb_strip}
+color1_argb=0x{color1.argb_strip}
+color2_argb=0x{color2.argb_strip}
+color3_argb=0x{color3.argb_strip}
+color4_argb=0x{color4.argb_strip}
+color5_argb=0x{color5.argb_strip}
+color6_argb=0x{color6.argb_strip}
+color7_argb=0x{color7.argb_strip}
+color8_argb=0x{color8.argb_strip}
+color9_argb=0x{color9.argb_strip}
+color10_argb=0x{color10.argb_strip}
+color11_argb=0x{color11.argb_strip}
+color12_argb=0x{color12.argb_strip}
+color13_argb=0x{color13.argb_strip}
+color14_argb=0x{color14.argb_strip}
+color15_argb=0x{color15.argb_strip}
