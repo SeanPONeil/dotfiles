ee4572ce5febdcb57a14df47f461fc03f1083378
Sean ONeil <oneilse14@gmail.com>
2022-04-26 13:22:17 -0400

    git-extras

--- .config/zsh/plugins.zsh
+++ .config/zsh/plugins.zsh
@@ -29,6 +29,7 @@ zplug "docker/cli", use:"contrib/completion/zsh/_docker"
 zplug "$ZDOTDIR/completions",             from:local
 if [[ $OSTYPE == *darwin* ]]; then
   zplug "/usr/local/share/zsh-completions", from:local, if:"[[ $OSTYPE == *darwin* ]]"
+  zplug "/usr/local/opt/git-extras/share/git-extras", from:local, if:"[[ $OSTYPE == *darwin* ]]"
 elif [[ $OSTYPE == *linux* ]]; then
   zplug "/usr/share/zsh/vendor-completions", from:local, if:"[[ $OSTYPE == *linux* ]]"
 fi
